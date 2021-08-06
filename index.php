1587f1d937052a85efed949a385c98734fc46b50
Ruang Koding <riky.hasibuan@gmail.com>
2021-08-06 14:10:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
 				<li><a href="https://ruangkoding.id/apa-itu-load-balancing/" target="_blank">apa itu load balancing</a></li>
 				<li><a href="https://ruangkoding.id/tutorial-crud-dengan-pdo-dan-mysql/" target="_blank">tutorial crud dengan pdo dan mysql</a></li>
 				<li><a href="https://ruangkoding.id/tutorial-cara-membuat-package-laravel-sendiri/" target="_blank">tutorial cara membuat package laravel sendiri</a></li>
+				<li><a href="https://ruangkoding.id/membuat-branch-baru-di-git/" target="_blank">membuat branch baru di git</a></li>
 			<ul>
 	  </div>
 	</main>
