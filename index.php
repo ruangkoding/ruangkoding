50bb3c5d2f9841adaaf04d08ba577de2896b7c41
Ruang Koding <riky.hasibuan@gmail.com>
2021-08-10 04:54:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="robots" content="index, follow">
 	<title>RuangKoding ID</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
